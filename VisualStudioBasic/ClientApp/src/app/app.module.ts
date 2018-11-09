284a73cc33d5c378031f5a836dd7604c99fa6f4e
Keith Aubin <kaubster@gmail.com>
2018-11-09 16:51:04 -0500

    init import of VisualStudioBasic, tested

---
+++ VisualStudioBasic/ClientApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import 'bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UiModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
